dd79ff656567a23534accb55bcc03f43425de4f4
Behzad Samadi <behzad.samadi@gmail.com>
2023-03-02 22:34:20 -0500

    v1.6.0 to AIRSIM_VERSION=v1.8.1

--- docker/download_blocks_env_binary.sh
+++ docker/download_blocks_env_binary.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -x
 
+AIRSIM_VERSION=v1.8.1
+
 if ! which unzip; then
     sudo apt-get install unzip
 fi
 
-wget -c https://github.com/microsoft/AirSim/releases/download/v1.6.0-linux/Blocks.zip 
+wget -c https://github.com/microsoft/AirSim/releases/download/${AIRSIM_VERSION}/Blocks.zip 
+
 unzip -q Blocks.zip
 rm Blocks.zip
